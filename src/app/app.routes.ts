b80da531307fac2560ec2498a7555357124c3a20
chirotAunkitti <chirotarm2546@gmail.com>
2025-02-12 09:45:48 +0700

    step-2-4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { provideRouter } from '@angular/router';
 import { SelectPDFComponent } from './select-pdf/select-pdf.component';
 import { CheckDocumentComponent } from './check-document/check-document.component';
+import { Step2Component } from './step-2/step-2.component';
+import { Step3Component } from './step-3/step-3.component';
+import { Step4Component } from './step-4/step-4.component';
 
 export const routes: Routes = [
   { 
@@ -11,9 +14,20 @@ export const routes: Routes = [
   },
   { 
     path: 'select-pdf', 
-    component: SelectPDFComponent },
+    component: SelectPDFComponent 
+  },
   { path: 'check-document', 
-    component: CheckDocumentComponent },
+    component: CheckDocumentComponent 
+  },
+  { path: 'step-2-pdf',  
+    component: Step2Component 
+  },
+  { path: 'step-3-pdf',  
+    component: Step3Component 
+  },
+  { path: 'step-4-pdf',  
+    component: Step4Component 
+  },
 
 ];
 
