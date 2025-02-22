a17c604ed92f5bae929f66a2a645b9d5508c0486
chirotAunkitti <116668609+chirotAunkitti@users.noreply.github.com>
2025-02-23 00:35:44 +0700

    eee

---
+++ src/app/step-7/step-7.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-7',
+  standalone: true,
+  imports: [],
+  templateUrl: './step-7.component.html',
+  styleUrl: './step-7.component.css'
+})
+export class Step7Component {
+
+}
